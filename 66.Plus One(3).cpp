ba498837a02df16c9ac0d21ed9d37799804ef3cb
QuantumCipherMaster <rogernigel60@gmail.com>
2024-04-28 21:04:25 +0800

    Create 66.Plus One(3).cpp

---
+++ 66.Plus One(3).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i] += 1;
+                return digits;
+            }
+        }
+        if (digits[0] == 0) {
+            digits[0] = 1;
+            digits.push_back(0);
+        }
+        return digits;
+    }
+};
