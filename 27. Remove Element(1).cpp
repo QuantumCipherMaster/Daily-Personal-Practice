e97c0977fc03ab9f14e0667243702fb6c9317c10
QuantumCipherMaster <rogernigel60@gmail.com>
2024-04-29 23:38:06 +0800

    Update 27. Remove Element(1).cpp

---
+++ 27. Remove Element(1).cpp
@@ -0,0 +1,13 @@
+// use two pointers to solve this problem
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.size(); fastIndex++) {
+            if (val != nums[fastIndex]) {
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+};
