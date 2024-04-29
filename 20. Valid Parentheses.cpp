cab649015ad5f012d0a53b524bd3c8766f1d2439
QuantumCipherMaster <rogernigel60@gmail.com>
2024-04-29 17:53:25 +0800

    Update 20. Valid Parentheses.cpp

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size() % 2 !=  0)
+            return false;
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(') st.push(')');
+            else if (s[i] == '{') st.push('}');
+            else if (s[i] == '[') st.push(']');
+            else if (st.empty() || st.top() != s[i]) return false;
+            else st.pop();
+        }
+        return st.empty();
+    }
+};
