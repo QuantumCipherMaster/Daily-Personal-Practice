abc8f133100d3ca9c68b78e4999c3007ee4a8b91
QuantumCipherMaster <rogernigel60@gmail.com>
2024-04-30 11:28:58 +0800

    Update 704. Binary Search(2).cpp

---
+++ 704. Binary Search(2).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size();
+        while (left < right) {
+            int middle = left + ((right - left) / 2);
+            if (nums[middle] < target) {
+                left = middle + 1;
+            } else if (nums[middle] > target) {
+                right = middle;
+            } else
+                return middle;
+        }
+        return -1;
+    }
+};
