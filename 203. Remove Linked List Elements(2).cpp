dd1220cc7253641d88d054f09bd5d198da5128ec
S.P. <128596662+QuantumCipherMaster@users.noreply.github.com>
2024-05-01 11:48:58 +0800

    Update 203. Remove Linked List Elements(2).cpp

---
+++ 203. Remove Linked List Elements(2).cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while (head != NULL && head->val == val) {
+            ListNode* tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+
+        ListNode* cur = head;
+        while (cur != NULL && cur->next != NULL) {
+            if (cur->next->val == val) {
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+            } else {
+                cur = cur->next;
+            }
+        }
+        return head;
+    }
+};
