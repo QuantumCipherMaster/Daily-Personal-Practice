8ad1533c203610395b8d41a8c0394f262520005a
QuantumCipherMaster <rogernigel60@gmail.com>
2024-05-02 11:11:42 +0800

    Update 206. Reverse Linked List(2).cpp

--- 206. Reverse Linked List(2).cpp
+++ 206. Reverse Linked List(2).cpp
@@ -1,1 +1,22 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* prev, ListNode* cur) {
+        if (cur == NULL) {
+            return prev;
+        }
+        ListNode* tmp = cur->next;
+        cur->next = prev;
+        return reverse(cur, tmp);
+    }
+    ListNode* reverseList(ListNode* head) { return reverse(NULL, head); }
+};
