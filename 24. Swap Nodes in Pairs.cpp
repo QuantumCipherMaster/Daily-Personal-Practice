b11e8a287970eaf92deb1e7f69f5dd822f3642ca
QuantumCipherMaster <rogernigel60@gmail.com>
2024-05-02 14:18:20 +0800

    Update 24. Swap Nodes in Pairs.cpp

---
+++ 24. Swap Nodes in Pairs.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummyHead = new ListNode(1);
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+        while (cur->next != nullptr && cur->next->next != nullptr) {
+            ListNode* tmp = cur->next;
+            ListNode* tmp1 = cur->next->next->next;
+
+            cur->next = cur->next->next;
+            cur->next->next = tmp;
+            cur->next->next->next = tmp1;
+            cur = cur->next->next;
+        }
+        ListNode* result = dummyHead->next;
+        delete dummyHead;
+        return result;
+    }
+};
