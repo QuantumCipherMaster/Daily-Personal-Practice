d932444f84ba00fc3d7bedfa764f28cadfebec83
S.P. <128596662+QuantumCipherMaster@users.noreply.github.com>
2024-05-03 22:43:10 +0800

    Update 242. Valid Anagram.cpp

---
+++ 242. Valid Anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int record[26];
+        for (int i = 0; i < s.size(); i++) {
+            record[s[i] - 'a']++;
+        }
+        for (int i = 0; i < t.size(); i++) {
+            record[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (record[i] != 0)
+                return false;
+        }
+        return true;
+    }
+};
